2d51cc201186535878323e37db2bdfdbde92c67b
roland-liedtke <liedtke.roland@gmail.com>
2022-08-21 14:47:03 +0200

    add active css

--- javascript.js
+++ javascript.js
@@ -1,8 +1,4 @@
 function onSubmit() {
     document.getElementById("card1").style.display = "none";
     document.getElementById("card2").style.display = "block";
-}
-
-function changeStarColor() {
-    document.getElementById('star-one').style.backgroundColor = '';
-}
+};
